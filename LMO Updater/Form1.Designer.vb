b50c7c2af1de76dcd42247fe165e671d3030b2b2
toaaa <toa@toaaa.de>
2023-02-19 20:50:21 +0100

    Initial commit

---
+++ LMO Updater/Form1.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 41.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 48)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(460, 78)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "LMO Updater"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI Semibold", 14.4!, System.Drawing.FontStyle.Bold)
+        Me.Label2.Location = New System.Drawing.Point(12, 23)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(460, 37)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Zu jeder Zeit Up-to-date mit"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label3
+        '
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI Semibold", 14.4!, System.Drawing.FontStyle.Bold)
+        Me.Label3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Label3.Location = New System.Drawing.Point(12, 126)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(460, 37)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Aktualisiere deine Initialisierung!"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.FromArgb(CType(CType(19, Byte), Integer), CType(CType(154, Byte), Integer), CType(CType(67, Byte), Integer))
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI Semibold", 12.4!, System.Drawing.FontStyle.Bold)
+        Me.Label4.Image = Global.LMO_Updater.My.Resources.Resources.Button
+        Me.Label4.Location = New System.Drawing.Point(128, 443)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(229, 60)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Aktualisierung " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Initialisieren"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 569)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(460, 30)
+        Me.ProgressBar1.TabIndex = 4
+        Me.ProgressBar1.Visible = False
+        '
+        'Label5
+        '
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI Semibold", 14.4!, System.Drawing.FontStyle.Bold)
+        Me.Label5.ForeColor = System.Drawing.Color.White
+        Me.Label5.Location = New System.Drawing.Point(12, 489)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(460, 34)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Lade Bytes Auf den Personalisierten Rechner..." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.Label5.Visible = False
+        '
+        'Label6
+        '
+        Me.Label6.Font = New System.Drawing.Font("Segoe UI Semibold", 14.4!, System.Drawing.FontStyle.Bold)
+        Me.Label6.ForeColor = System.Drawing.Color.White
+        Me.Label6.Location = New System.Drawing.Point(7, 523)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(460, 34)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "0 MB / 0 MB"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.Label6.Visible = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 2000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(20, Byte), Integer), CType(CType(20, Byte), Integer), CType(CType(20, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(484, 611)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Segoe UI Semibold", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.Color.White
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "LMO Updater"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+End Class
---
+++ LMO Updater/Form1.vb
@@ -0,0 +1,66 @@
+﻿Imports System.ComponentModel
+Imports System.IO.Compression
+Imports System.Net
+
+Public Class Form1
+
+    Private Sub Label4_MouseEnter(sender As Object, e As EventArgs) Handles Label4.MouseEnter
+        Label4.BackColor = Color.FromArgb(16, 163, 93)
+    End Sub
+
+    Private Sub Label4_MouseLeave(sender As Object, e As EventArgs) Handles Label4.MouseLeave
+        Label4.BackColor = Color.FromArgb(19, 154, 67)
+    End Sub
+    Private Sub Label4_MouseUp(sender As Object, e As EventArgs) Handles Label4.MouseUp
+        Label4.BackColor = Color.FromArgb(16, 163, 93)
+        Label4.Visible = False
+        Label6.Visible = True
+        Label5.Visible = True
+        ProgressBar1.Visible = True
+
+        If My.Computer.FileSystem.FileExists("latestD.zip") Then
+            My.Computer.FileSystem.DeleteFile("latestD.zip")
+        End If
+
+        Dim wc As New WebClient
+        AddHandler wc.DownloadProgressChanged, AddressOf ProgChanged
+        AddHandler wc.DownloadFileCompleted, AddressOf ProgDone
+        wc.DownloadFileAsync(New Uri("http://www.cartaux.net/LMO/latest.zip"), "latestD.zip")
+        'It is used to download a file from the specified URl to the destination
+    End Sub
+
+    Private Sub ProgDone(sender As Object, e As AsyncCompletedEventArgs)
+        ProgressBar1.Value = 100
+        Label5.Text = "Extrahiere komprimierte Bytes aus dem Archiv..."
+        Label6.Text = ""
+
+        ZipFile.ExtractToDirectory("latestD.zip", Application.StartupPath())
+
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub ProgChanged(sender As Object, e As DownloadProgressChangedEventArgs)
+        ProgressBar1.Value = e.ProgressPercentage
+        Label6.Text = String.Format("{0} MB / {1} MB", (e.BytesReceived / 1024D / 1024D.ToString()), (e.TotalBytesToReceive / 1024D / 1024D.ToString()))
+    End Sub
+
+    Private Sub Label4_MouseDown(sender As Object, e As EventArgs) Handles Label4.MouseDown
+        Label4.BackColor = Color.FromArgb(13, 171, 118)
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If My.Computer.FileSystem.FileExists("Laise Moat Online.exe") Then
+
+            Label5.Text = "Aktualisierung erfolgreich vereinsamt ausgeführt!"
+            Label5.ForeColor = Color.FromArgb(19, 154, 67)
+            ProgressBar1.Visible = False
+            Timer2.Enabled = True
+            Timer1.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        My.Computer.FileSystem.DeleteFile("latestD.zip")
+        End
+    End Sub
+End Class
